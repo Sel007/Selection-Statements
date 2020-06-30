556489d8c9123feb99066e912afb6135bc41b756
Sel007 <selina.08225.1@gmail.com>
2020-06-30 13:53:49 -0400

    Finished

--- SelectionStatementsConsoleUI/Program.cs
+++ SelectionStatementsConsoleUI/Program.cs
@@ -6,7 +6,49 @@ namespace SelectionStatementsConsoleUI
     {
         static void Main(string[] args)
         {
-            
+            int FavoriteNumber = 11;
+
+            Console.WriteLine("Guess my favorite number. Hint: below 20");
+            int userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < FavoriteNumber)
+            {
+                Console.WriteLine("Too Low!! :(");
+            }
+
+            else if (userGuess > FavoriteNumber)
+            {
+                Console.WriteLine("Too High!! Try again!");
+            }
+
+            else if (userGuess == FavoriteNumber)
+            {
+                Console.WriteLine("Congratulations!! You got it!!! :) Here's a free ticket.");
+            }
+
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+            string subject = "C#";
+
+            Console.WriteLine("Choose a subject");
+            string userSubject = Console.ReadLine();
+
+
+            switch (userSubject)
+            {
+                case "C#":
+                    Console.WriteLine("You are in the right class!");
+                    break;
+                case "Java":
+                    Console.WriteLine("C# is better than Java");
+                    break;
+                default:
+                    Console.WriteLine("We learn to code the right way!");
+                    break;
+            }
         }
     }
 }
